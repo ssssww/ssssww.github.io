ed625cbcba6dbefd5b53ab66e221be9344ef663b
ssssww <ssssww@hotmail.com>
2016-12-05 14:18:41 -0800

    try google map with api key

--- script/script.js
+++ script/script.js
@@ -376,7 +376,7 @@ function geoFindMe() {
 			+ "&zoom=15&size=300x300&maptpe=roadmap"
 			+ "&markers=color:red%7Clabel:C%7C" + latitude + "," + longitude
 			+ "&ke" + api;
-
+img.src= "https://maps.googleapis.com/maps/api/staticmap?center=49.231529599999995,-123.11862469999998&zoom=15&size=300x300&maptpe=roadmap&markers=color:red%7Clabel:C%7C49.231529599999995,-123.11862469999998&keAIzaSyAcSOiD76Tstt0E99112MmyfTczbOHvNIM";
 		output.appendChild(img);
 	}
 
