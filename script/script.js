4695f0fb70222829cc7daf15f8adc2f456f7873e
ssssww <ssssww@hotmail.com>
2016-12-05 14:12:05 -0800

    try google map with api key? take 4

--- script/script.js
+++ script/script.js
@@ -376,7 +376,7 @@ function geoFindMe() {
 			+ "&zoom=13&size=300x300&maptpe=roadmap"
 			+ "&markers=color:red%7Clabel:C%7C" + latitude + "," + longitude
 			+ "&key=" + api;
-		
+img.src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318 &markers=color:red%7Clabel:C%7C40.718217,-73.998284 &key="+api;
 		output.appendChild(img);
 	}
 
