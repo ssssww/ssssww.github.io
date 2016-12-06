153fd080c872c76ffb85d0c6f18fe9b253923521
ssssww <ssssww@hotmail.com>
2016-12-06 09:55:05 -0800

    get nearby busstop json

--- script/script.js
+++ script/script.js
@@ -400,7 +400,7 @@ function geoFindMe() {
 				+ markerCoordinates
 				+ "&key=" + googleMapApi;
 
-			output.appendChild(img);
+			output.append(img);
 		}//test
 
 		$.getJSON( busStopURL, test )
