8f7cef01a4d19053cf07606825482a41e77c032c
ssssww <ssssww@hotmail.com>
2016-12-06 15:43:50 -0800

    set marker info;?

--- script/script.js
+++ script/script.js
@@ -396,7 +396,7 @@ function geoFindMe() {
 		{
 			var youAreHere = {"lat": latitude, "lng": longitude};
 			var mapOptions = {
-				zoom: 20,
+				zoom: 17,
 				center: youAreHere,
 				mapTypeId: 'roadmap'
 			};
